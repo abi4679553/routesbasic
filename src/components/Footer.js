2971cb49fed7313c3cbfc28ce63df4aa62da8a57
abi4679553 <abivengadajalam7708@gmail.com>
2025-12-12 12:39:13 +0530

    update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        <p className='p-5 text-2xl bg-green-500 text-white'> Footer </p>
+    <div className='p-5 text-2xl bg-green-500 text-white flex gap-10 justify-between'>
+        <p> Footer </p>
+        <p>Whats app</p>
+        <p>Insta</p>
+        <p>contact</p>
     </div>
   )
 }
