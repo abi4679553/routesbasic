a08dca8b44cd44ca090e5e840b4e48cb56224cc3
abi4679553 <abivengadajalam7708@gmail.com>
2025-12-15 19:17:33 +0530

    update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='p-5 text-2xl bg-green-500 text-white flex gap-10 justify-between'>
+    <div className='p-5 text-2xl bg-red-700 text-white flex gap-10 justify-between'>
         <p> Footer </p>
         <p>Whats app</p>
         <p>Insta</p>
--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -8,9 +8,9 @@ import { IoIosHeartEmpty } from "react-icons/io";
 export default function FrontPage() {
   return (
     <div>
-        <div className='flex  items-center gap-2 '>
+        <div className='flex  items-center gap-2  text-2xl '>
         <IoIosHeartEmpty />
-        <p>wjhcjqll</p>
+        <p>Shop </p>
         </div>
         <p className='text-3xl text-blue-700  text-center m-10'>Welcome My WebPage !!</p>
         <img src={img1}  alt='img1' className='h-[300px] w-80  ml-[40%] ' />
